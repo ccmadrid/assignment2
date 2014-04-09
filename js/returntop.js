b35f091a9bd645869ca2256fc610efdce84507bb
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-04-09 18:53:52 -0400

    check

--- js/returntop.js
+++ js/returntop.js
@@ -1,29 +1,9 @@
-var gotop = document.querySelector('.button');
+document.querySelector('.rtt').addEventListener('click', function (e) {
 
-// Wait for the user to click on the nav button
- gotop.addEventListener('click', function (e) {
-  // Javascript passes an event object for event listeners
-  // It's being captured in the `e` argument above
-
-  // Since .nav-btn is a link, by default it navigate somewhere
-  // `e.preventDefault()` will stop the link from doing what it normally does
-  e.preventDefault();
-
-  // Will toggle @data-state on .nav-top and .nav-btn
-  // @data-state is being used in CSS to style what the nav elements will look like
-
-document.ready(function() {
-    /*window.scroll(function () {
-       if (this.scrollTop() > 200) {
-           gotop.fadeIn(200);
-       }else{
-           gotop.fadeOut(200);
-       }
-    });
-    */
-    gotop.click(function(event) {
-        event.preventDefault();
+    e.preventDefault();
         
-        $('html, body').animate({scrollTop: 0},300);
-    })
+  $('html, body').animate({ scrollTop: 0 }, 300);
+  return false;
+
 });
+
