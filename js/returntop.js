c5535b7b03918ac2ead083ab62b11c10e97f097e
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-04-15 02:20:02 -0400

    jquery

--- js/returntop.js
+++ js/returntop.js
@@ -1,8 +1,25 @@
-document.querySelector('.rtt').addEventListener('click', function (e) {
+$(document).ready(function(){
 
-   // e.preventDefault();
-        
-  $('html, body').animate({ scrollTop: 0 }, 300);
-  return false;
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
 
 });
