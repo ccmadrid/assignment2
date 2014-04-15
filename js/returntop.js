ba404662939537b9bfd8b9fea7004151803a2759
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-04-15 02:32:42 -0400

    rtt

--- js/returntop.js
+++ js/returntop.js
@@ -1,25 +1,7 @@
-$(document).ready(function(){
-
-	// hide #back-top first
-	$("#back-top").hide();
-	
-	// fade in #back-top
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
-
-		// scroll body to 0px on click
-		$('#back-top a').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	});
-
+$(function () {
+    $('body').on("click", ".rtt", function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
 });
