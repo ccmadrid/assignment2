ae0f9b6a0d90009e091296a3031253a532c79759
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-04-22 01:28:45 -0400

    js

--- js/returntop.js
+++ js/returntop.js
@@ -1,4 +1,16 @@
-var speed = 800;
+var speed = 750;
+var fade = 500;
+        
+$(document).ready(function(){        
+    $(window).bind('scroll', function(){
+        if ($(this).scrollTop() > 150) {
+            $('.rtt').fadeIn(fade);
+        } else {
+            $('.rtt').fadeOut(fade);
+        }
+    });
+});
+
 $(function () {
     $('body').on("click", ".rtt", function (e) {
         
